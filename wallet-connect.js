e303926ad4f71476a38d17147e23556419293577
asalft <telebotimport@gmail.com>
2025-05-02 05:55:12 +0300

    Update wallet-connect.js

--- wallet-connect.js
+++ wallet-connect.js
@@ -1,4 +1,17 @@
+// تأكد من أنك أضفت مكتبة tonconnect-ui.min.js في HTML قبل هذا الملف
+// <script src="https://unpkg.com/@tonconnect/ui@latest/dist/tonconnect-ui.min.js"></script>
+
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
   manifestUrl: 'https://asalft.github.io/Asal/tonconnect-manifest.json',
-  buttonRootId: 'ton-connect',
+  buttonRootId: 'ton-connect'
+});
+
+// مثال: يمكنك مراقبة حالة الاتصال هنا
+tonConnectUI.onStatusChange(async (walletInfo) => {
+  if (walletInfo) {
+    console.log('تم الاتصال بالمحفظة:', walletInfo);
+    alert('تم الاتصال بمحفظتك بنجاح!');
+  } else {
+    console.log('لم يتم الاتصال بعد.');
+  }
 });
